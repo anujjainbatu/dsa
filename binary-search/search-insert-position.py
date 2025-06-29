d1b8d40342e20de8008fcd89ed21a4fa9433c235
Anuj Jain <anujjainbatu@gmail.com>
2025-06-29 22:02:18 +0530

    Update search-insert-position.py

--- binary-search/search-insert-position.py
+++ binary-search/search-insert-position.py
@@ -5,15 +5,15 @@ class Solution:
         n = len(nums)
         l = 0
         r = n-1
+        lower_bound = n
 
         while r >= l:
             mid = (l+r)//2
 
-            if target < nums[mid]:
+            if target <= nums[mid]:
+                lower_bound = mid
                 r = mid-1
-            elif target > nums[mid]:
-                l = mid+1
             else:
-                return mid
+                l = mid+1
 
-        return l
+        return lower_bound

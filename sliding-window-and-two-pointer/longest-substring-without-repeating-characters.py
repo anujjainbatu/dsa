6e3f900a1932e4d76826d3c5429579f69a891d3b
Anuj Jain <anujjainbatu@gmail.com>
2025-07-23 11:04:06 +0530

    Update longest-substring-without-repeating-characters.py

--- sliding-window-and-two-pointer/longest-substring-without-repeating-characters.py
+++ sliding-window-and-two-pointer/longest-substring-without-repeating-characters.py
@@ -1,1 +1,15 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters
+# O(n^2), O(n)
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        maximum = 0
+        for i in range(n):
+            my_set = set()
+            for j in range(i,n):
+                if s[j] in my_set:
+                    break
+                maximum = max(maximum,j-i+1)
+                my_set.add(s[j])
+
+        return maximum

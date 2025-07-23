fae2cba7492d9d2f2b854d0c507d5893e7f7aa48
Anuj Jain <anujjainbatu@gmail.com>
2025-07-23 11:20:11 +0530

    Update longest-substring-without-repeating-characters.py

--- sliding-window-and-two-pointer/longest-substring-without-repeating-characters.py
+++ sliding-window-and-two-pointer/longest-substring-without-repeating-characters.py
@@ -1,4 +1,20 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters
+# O(n), O(n), using sliding window & two pointer
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        n = len(s)
+        maximum = 0
+        l, r = 0, 0
+        my_dict = {}
+        while r < n:
+            if s[r] in my_dict:
+                l = max(l,my_dict[s[r]]+1)
+            maximum = max(maximum, r-l+1)
+            my_dict[s[r]] = r
+            r += 1
+
+        return maximum
+        
 # O(n^2), O(n)
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
